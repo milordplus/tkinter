34bd7766c0e0f3e0493aa52b9126696b8dec767d
milordplus <milordplus@mail.ru>
2023-11-01 00:52:54 +0300

    08

--- 01/Notepad.py
+++ 01/Notepad.py
@@ -1,13 +1,22 @@
 from tkinter import *
 
+
+def change_theme(theme):
+    text_field['bg'] = view_color[theme]['text_bg']
+    text_field['fg'] = view_color[theme]['text_fg']
+    text_field['insertbackground'] = view_color[theme]['cursor']
+    text_field['selectbackground'] = view_color[theme]['select_bg']
+
+
+def change_font(font):
+    text_field['font'] = fonts[font]['font']
+
+
 root = Tk()
 
 root.title('Блокнот')
 root.geometry('600x700')
 root.iconbitmap('notepad.png')
-
-f_text = Frame(root)
-f_text.pack(fill=BOTH, expand=1)
 
 main_menu = Menu(root)
 
@@ -23,21 +32,44 @@ root.config(menu=file_menu)
 view_menu = Menu(main_menu, tearoff=0)
 view_menu_sub = Menu(view_menu, tearoff=0)
 font_menu_sub = Menu(view_menu, tearoff=0)
-view_menu_sub.add_command(label='Темная')
-view_menu_sub.add_command(label='Светлая')
+view_menu_sub.add_command(label='Темная', command=lambda: change_theme('dark'))
+view_menu_sub.add_command(label='Светлая', command=lambda: change_theme('light'))
 view_menu.add_cascade(label='Тема', menu=view_menu_sub)
 
-font_menu_sub.add_command(label='Arial')
-font_menu_sub.add_command(label='Comic Sans MS')
-font_menu_sub.add_command(label='Times New Roman')
+font_menu_sub.add_command(label='Arial', command=lambda: change_font('Arial'))
+font_menu_sub.add_command(label='Comic Sans MS', command=lambda: change_font('CSMS'))
+font_menu_sub.add_command(label='Times New Roman', command=lambda: change_font('TNR'))
 view_menu.add_cascade(label='Шрифт...', menu=font_menu_sub)
 root.config(menu=view_menu)
 
 # Добавление списков меню
 main_menu.add_cascade(label='Файл', menu=file_menu)
 main_menu.add_cascade(label='Вид', menu=view_menu)
-
 root.config(menu=main_menu)
+
+f_text = Frame(root)
+f_text.pack(fill=BOTH, expand=1)
+
+view_color = {
+    'dark': {
+        'text_bg': 'black', 'text_fg': 'lime', 'cursor': 'brown', 'select_bg': '#8D917A'
+    },
+    'light': {
+        'text_bg': 'white', 'text_fg': 'black', 'cursor': '#A5A5A5', 'select_bg': '#FAEEDD'
+    }
+}
+
+fonts = {
+    'Arial': {
+        'font': 'Arial 14 bold'
+    },
+    'CSMS': {
+        'font': ('Comic Sans MS', 14, 'bold')
+    },
+    'TNR': {
+        'font': ('Times New Roman', 14, 'bold')
+    }
+}
 
 text_field = Text(f_text,
                   bg='black',
@@ -48,7 +80,8 @@ text_field = Text(f_text,
                   insertbackground='white',
                   selectbackground='#8D917A',
                   spacing3='10',
-                  width=30
+                  width=30,
+                  font='Arial 14 bold'
                   )
 text_field.pack(expand=1, fill=BOTH, side=LEFT)
 
